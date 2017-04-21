29e22d67b68719b017cd5156e7fda98a3b754458
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:21:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+SkyTime
+========
+
+Educational game for Sugar
+
+This game is designed to teach younger children the connection between clocks and time as well as how to read an analog clock.  
+
+The game currently has three different language settings: English, Spanish, and French.
+
+There are three different menu settings: Play, Challenge, and Tutorial.
+
+Play: This game mode has the player interact with the clock hands to match their displayed time with the goal time displayed.
+If the play does this correctly, a sun is displayed and the player is awarded one sun.  We plan on implementing a reward system
+to give meaning to the suns you are awarded.  These suns will turn into a currency where players can purchase new themes for the game
+and customize almost everything!
+
+Challenge: This game mode is has the same exact concept of the 'Play' game mode, execpt here your time is not displayed, only the
+goal time.  This creates more of a challenge for children to tell time only by reading the clock rather than comparing their displayed
+time and the goal time and adjusting the hands until the two are identical.
+
+Tutorial: This screen just shows the layout of the XO laptop keyboard and what buttons the player will need to use to play the game.
---
+++ constants.py
@@ -0,0 +1,253 @@
+width = 1200
+height = 850
+
+clock_render_left = width * .015
+clock_render_top = height * .01
+
+box_render_left = width * .55
+time_render_left = box_render_left + (width * .185)
+your_time_render_top = height * .05
+goal_time_render_top = height * .285
+
+REWARD_OPTIONS = ['Clock Faces', 'Backgrounds']
+
+MENU_OPTIONS = ['Play', 'Challenge', 'How To Play', 'Rewards']
+
+CLOCK_REWARDS = ['default', 'green', 'moon', 'purple',
+                 'watermelon', 'spikeysun', 'earth', 'bluemoon']
+
+BACKGROUND_REWARDS = ['default', 'night-cloudy', 'night-clear',
+                      'rainbow', 'sunset', 'rain', 'galaxy', 'northern-lights']
+
+REWARDS_DICT = {
+    'clock': {
+        'default': {
+            'name': 'default',
+            'earned': True,
+            'value': '0',
+            'color': (0, 0, 0)
+        },
+        'green': {
+            'name': 'green',
+            'earned': False,
+            'value': '15',
+            'color': (0, 0, 0)
+        },
+        'moon': {
+            'name': 'moon',
+            'earned': False,
+            'value': '30',
+            'color': (0, 0, 0)
+        },
+        'purple': {
+            'name': 'purple',
+            'earned': False,
+            'value': '75',
+            'color': (0, 0, 0)
+        },
+        'watermelon': {
+            'name': 'watermelon',
+            'earned': False,
+            'value': '120',
+            'color': (0, 0, 0)
+        },
+        'spikeysun': {
+            'name': 'spikeysun',
+            'earned': False,
+            'value': '175',
+            'color': (0, 0, 0)
+        },
+        'bluemoon': {
+            'name': 'bluemoon',
+            'earned': False,
+            'value': '300',
+            'color': (0, 0, 0)
+        },
+        'earth': {
+            'name': 'earth',
+            'earned': False,
+            'value': '225',
+            'color': (0, 0, 0)
+        }
+    },
+    'background': {
+        'default': {
+            'name': 'default',
+            'earned': True,
+            'value': '0',
+            'color': (0, 0, 0)
+        },
+        'night-cloudy': {
+            'name': 'night-cloudy',
+            'earned': False,
+            'value': '15',
+            'color': (255, 255, 255)
+        },
+        'night-clear': {
+            'name': 'night-clear',
+            'earned': False,
+            'value': '30',
+            'color': (255, 255, 255)
+        },
+        'rainbow': {
+            'name': 'rainbow',
+            'earned': False,
+            'value': '75',
+            'color': (0, 0, 0)
+        },
+        'sunset': {
+            'name': 'sunset',
+            'earned': False,
+            'value': '125',
+            'color': (0, 0, 0)
+        },
+        'rain': {
+            'name': 'rain',
+            'earned': False,
+            'value': '175',
+            'color': (0, 0, 0)
+        },
+        'northern-lights': {
+            'name': 'northern-lights',
+            'earned': False,
+            'value': '300',
+            'color': (255, 255, 255)
+        },
+        'galaxy': {
+            'name': 'galaxy',
+            'earned': False,
+            'value': '225',
+            'color': (255, 255, 255)
+        }
+    },
+    'score': '0',
+    'number_clocks': 0,
+    'number_backgrounds': 0
+}
+
+HANDS = {
+    'minute': {
+        0: {
+            'image': None,
+            'render_left': clock_render_left + (width * .23),
+            'render_top': clock_render_top + (height * .13)
+        },
+        5: {
+            'image': None,
+            'render_left': clock_render_left + (width * .23),
+            'render_top': clock_render_top + (height * .145)
+        },
+        10: {
+            'image': None,
+            'render_left': clock_render_left + (width * .23),
+            'render_top': clock_render_top + (height * .215)
+        },
+        15: {
+            'image': None,
+            'render_left': clock_render_left + (width * .25),
+            'render_top': clock_render_top + (height * .31)
+        },
+        20: {
+            'image': None,
+            'render_left': clock_render_left + (width * .2425),
+            'render_top': clock_render_top + (height * .31)
+        },
+        25: {
+            'image': None,
+            'render_left': clock_render_left + (width * .225),
+            'render_top': clock_render_top + (height * .305)
+        },
+        30: {
+            'image': None,
+            'render_left': clock_render_left + (width * .23),
+            'render_top': clock_render_top + (height * .33)
+        },
+        35: {
+            'image': None,
+            'render_left': clock_render_left + (width * .162),
+            'render_top': clock_render_top + (height * .32)
+        },
+        40: {
+            'image': None,
+            'render_left': clock_render_left + (width * .12),
+            'render_top': clock_render_top + (height * .3)
+        },
+        45: {
+            'image': None,
+            'render_left': clock_render_left + (width * .105),
+            'render_top': clock_render_top + (height * .313)
+        },
+        50: {
+            'image': None,
+            'render_left': clock_render_left + (width * .12),
+            'render_top': clock_render_top + (height * .213)
+        },
+        55: {
+            'image': None,
+            'render_left': clock_render_left + (width * .16),
+            'render_top': clock_render_top + (height * .15)
+        }
+    },
+    'hour': {
+        0: {
+            'image': None,
+            'render_left': clock_render_left + (width * .2325),
+            'render_top': clock_render_top + (height * .19)
+        },
+        1: {
+            'image': None,
+            'render_left': clock_render_left + (width * .24),
+            'render_top': clock_render_top + (height * .2)
+        },
+        2: {
+            'image': None,
+            'render_left': clock_render_left + (width * .245),
+            'render_top': clock_render_top + (height * .25)
+        },
+        3: {
+            'image': None,
+            'render_left': clock_render_left + (width * .26),
+            'render_top': clock_render_top + (height * .3135)
+        },
+        4: {
+            'image': None,
+            'render_left': clock_render_left + (width * .245),
+            'render_top': clock_render_top + (height * .325)
+        },
+        5: {
+            'image': None,
+            'render_left': clock_render_left + (width * .235),
+            'render_top': clock_render_top + (height * .325)
+        },
+        6: {
+            'image': None,
+            'render_left': clock_render_left + (width * .23),
+            'render_top': clock_render_top + (height * .34)
+        },
+        7: {
+            'image': None,
+            'render_left': clock_render_left + (width * .185),
+            'render_top': clock_render_top + (height * .33)
+        },
+        8: {
+            'image': None,
+            'render_left': clock_render_left + (width * .145),
+            'render_top': clock_render_top + (height * .31)
+        },
+        9: {
+            'image': None,
+            'render_left': clock_render_left + (width * .145),
+            'render_top': clock_render_top + (height * .31)
+        },
+        10: {
+            'image': None,
+            'render_left': clock_render_left + (width * .15),
+            'render_top': clock_render_top + (height * .235)
+        },
+        11: {
+            'image': None,
+            'render_left': clock_render_left + (width * .18),
+            'render_top': clock_render_top + (height * .2)
+        }
+    }
+}
